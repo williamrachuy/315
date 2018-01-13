cad10d05322fc37ce2cce60e9f7eaecf4e5377e6
williamrachuy <wrachuy@calpoly.edu>
2018-01-12 17:05:53 -0800

    commit

--- push315.sh
+++ push315.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/bash
+set username "williamrachuy"
+set password "re7ained"
 
 git add -A ~/315
 git commit -m "commit"
 git push
+expect "Username for 'https://github.com': "
+send "$username"
+expect "Password for 'https://williamrachuy@github.com': "
+send "$password"
