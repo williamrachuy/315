221d08c7d2a8c7ae676394055fc6b605f251c391
williamrachuy <wrachuy@calpoly.edu>
2018-01-17 16:00:01 -0800

    commit

--- labs/lab2/main.c
+++ labs/lab2/main.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
-unsigned int umultiply(unsigned int a, unsigned int b) {
+unsigned int uMultiply(unsigned int a, unsigned int b) {
    unsigned int product = 0, i;
 
    for (i = 0; i < 16; i++) {
       if ((a >> i) & 0x0001)
          product += (b << i);
-         printf("%u, 0x%016X\n", i, product); 
    }
 
    return product;
@@ -21,10 +20,10 @@ unsigned int fltConv32(float g) {
 int main(void) {
    unsigned int testInt;
 
-   //fltConv32(3.14);
-   //fltConv32(3.15);
+   fltConv32(3.14);
+   fltConv32(3.15);
    
-   testInt = umultiply(0x0025, 0x0051);
+   testInt = uMultiply(0x0025, 0x0051);
    printf("0x%016X\n", testInt);   
 
    return 0;
