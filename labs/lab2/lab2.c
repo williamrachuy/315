4ca02813897c3a90a450c78bda9f5a020eedc6ea
williamrachuy <wrachuy@calpoly.edu>
2018-01-22 15:40:50 -0800

    commit

--- labs/lab2/lab2.c
+++ labs/lab2/lab2.c
@@ -23,6 +23,10 @@ unsigned int uMultiply(unsigned int a, unsigned int b) {
 intFloat *extFloat(intFloat *fltStruct, float flt) {
    unsigned int fltConv = (unsigned int)*(unsigned int *)&flt;
 
+#ifdef TRACE
+   printf("(extFloat) fltConv: 0x%08X\n", fltConv);
+#endif
+
    if (flt == 0) {
       fltStruct->sign     = 0;
       fltStruct->exponent = 0;
@@ -31,7 +35,7 @@ intFloat *extFloat(intFloat *fltStruct, float flt) {
       fltStruct->sign     = fltConv & 0x80000000;
       fltStruct->exponent = ((fltConv >> 23) & 0x000000FF) - 127;
       fltStruct->fraction = ((fltConv << 7) & 0x3FFFFF80) | 0x40000000;
-      if (fltStruct->sign) fltStruct->fraction = -fltStruct->fraction;
+      if (fltStruct->sign == 0x80000000) fltStruct->fraction = -fltStruct->fraction;
    }
 
 #ifdef TRACE
@@ -45,6 +49,7 @@ intFloat *extFloat(intFloat *fltStruct, float flt) {
 float packFloat(intFloat *fltStruct) {
    unsigned int fltConv = 0;
 
+   fltConv |= fltStruct->sign & 0x80000000;
    fltConv |= ((fltStruct->exponent + 127) << 23) & 0x7F800000;
    if (fltStruct->sign) fltStruct->fraction = -fltStruct->fraction;
    fltConv |= ((fltStruct->fraction & ~0x40000000) >> 7) & 0x007FFFFF;
@@ -91,7 +96,9 @@ float addFloat(float a, float b) {
    intFloat fltStructA, fltStructB, fltStructR;
    int exponentDiff;
 
-   fltStructR->sign = 0;
+   fltStructR.sign     = 0;
+   fltStructR.exponent = 0;
+   fltStructR.fraction = 0;
    extFloat(&fltStructA, a);
    extFloat(&fltStructB, b);
    exponentDiff = fltStructA.exponent - fltStructB.exponent;
